212498eab7d3e13de1e85b515437c72d65113ee5
Johan Larsson <karmajohan@gmail.com>
2023-09-22 22:01:36 +0200

    updates

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -6,13 +6,46 @@
     <div class="flex items-center justify-center flex-col">
       <img
         src="/img/johan.jpeg"
-        class="w-28 rounded-full mb-8 hover:opacity-80"
+        class="w-28 rounded-full mb-6 hover:opacity-80"
         alt="portraite - johan larsson"
       />
-      <div class="px-4">Johan Larsson</div>
-      <div class="px-4">karmajohan@gmail.com</div>
-      <div class="px-4"><a href="http://github.com/xljoha">Github</a></div>
+      <div class="px-4 flex justify-between items-center">
+        <span>Johan Larsson</span>
+      </div>
+      <div class="px-4 flex items-center justify-between">
+        <svg
+          class="mr-2"
+          xmlns="http://www.w3.org/2000/svg"
+          width="15"
+          height="15"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M19.07 13.88L13 19.94V22h2.06l6.06-6.07m1.58-2.35l-1.28-1.28a.517.517 0 0 0-.38-.17c-.15.01-.29.06-.39.17l-1 1l2.05 2l1-1c.19-.2.19-.52 0-.72M11 18H4V8l8 5l8-5v2h2V6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h7v-2m9-12l-8 5l-8-5h16Z"
+          />
+        </svg>
+        <span>karmajohan@gmail.com</span>
+      </div>
+      <div class="px-4 flex justify-between items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="mr-2"
+          width="15"
+          height="15"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
+          />
+        </svg>
+        <span><a href="http://github.com/xljoha">Github</a></span>
+      </div>
       <div class="px-4"><a href="tel:9051290512">+4676618116</a></div>
+      <div class="px-4 text-xs mt-4">
+        Built with Nuxt 3, Nuxt Content and Tailwind
+      </div>
     </div>
   </div>
 </template>
